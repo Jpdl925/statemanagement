c91e2cd23ceb8c4cf0449b72ea07b602f47b186f
Jpdl925 <jtpg925@gmail.com>
2024-10-23 21:02:32 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Counter from "./components/Counter"
+import Todo from "./components/Todo"
+import Login from './components/Login'
+
+const App = () => {
+  return (
+  <>
+  <h1 className="text-center">Global State Management</h1>
+
+  
+  {/* <Counter/> */}
+  {/* <Todo/> */}
+  <Login/>
+  </>
+  )
+}
+
+export default App
